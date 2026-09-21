f7ae3c69f001bd4b71e55e41f94ab4f69ae3b66f
lokesh <lokesh.77664@gmail.com>
2026-09-21 11:33:22 +0530

    modified struck

--- 1dl.c
+++ 1dl.c
@@ -1,26 +1,23 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<string.h>
-struct  student
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+struct student
 {
-char name[50];
- int rollno ; 
- float marks  ;   /* data */
+    char name[50];
+    int rollno;
+    float marks; /* data */
 };
 
-int main() {
-struct student s1;
-strcpy(s1.name , " lokesh");
+int main()
+{
+    struct student s1;
+    printf("enter ur  name marks roll no ");
 
-s1.rollno = 106;
-s1.marks = 7.8;
-
-
-
-
-
-
+    scanf("%s", s1.name);
+    scanf("%d", &s1.rollno);
+    scanf("%f", &s1.marks);
+    printf("%s %d %f", s1.name, &s1.rollno, &s1.marks);
 
     return 0;
 }
